73a2b3d7ebd740d4fce6b72774c566691f8c4039
Suvan <suvanramani@gmail.com>
2018-07-23 17:11:39 -0700

    Add comments

--- unit8_javascript_basics/06simpleTodo/script.js
+++ unit8_javascript_basics/06simpleTodo/script.js
@@ -1,6 +1,8 @@
 var action = "";
 var todos = new Array();
 
+// This is necessary to allow the html to load before
+// the loop begins
 window.setTimeout(function(){
     while (action !== "quit") {
         action = prompt("What would you like to do?");
