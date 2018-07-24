00d0f544d97bad95f5e4ef2f2cdd00b9f5d0b017
Suvan <suvanramani@gmail.com>
2018-07-24 12:01:50 -0700

    Impelent solution with classList.toggle()

--- unit9_domManipulation/04colourChanger/script.js
+++ unit9_domManipulation/04colourChanger/script.js
@@ -1,15 +1,29 @@
+// MY SOLUTION
+
+// var button = document.querySelector("button");
+// var body = document.querySelector("body");
+// var isPurple = false;
+
+// button.addEventListener("click", changeColour)
+
+// function changeColour() {
+//     if (isPurple) {
+//         body.style.background = "white";
+//     } else {
+//         body.style.background = "purple";
+//     }
+
+//     isPurple = !isPurple
+// }
+
+
+// *****************************
+
+// ALTERNATIVE SOLUTION
+
 var button = document.querySelector("button");
-var body = document.querySelector("body");
-var isPurple = false;
 
-button.addEventListener("click", changeColour)
-
-function changeColour() {
-    if (isPurple) {
-        body.style.background = "white";
-    } else {
-        body.style.background = "purple";
-    }
-
-    isPurple = !isPurple
-}
+button.addEventListener("click", function(){
+    // 'purple' class was defined in style tag of index.html
+    document.body.classList.toggle("purple");
+});
