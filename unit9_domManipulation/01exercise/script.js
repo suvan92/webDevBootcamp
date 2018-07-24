8ecbfb76c8bfc60ab718db7e6445eb67849ae9d7
Suvan <suvanramani@gmail.com>
2018-07-24 11:47:30 -0700

    Finish exercise 1

---
+++ unit9_domManipulation/01exercise/script.js
@@ -0,0 +1,16 @@
+var h1 = document.querySelector("h1");
+h1.style.color = "pink";
+
+var body = document.querySelector("body");
+
+var isBlue = false
+
+setInterval(function() {
+    if (isBlue) {
+        body.style.background = "white";
+    } else {
+        body.style.background = "green";
+    }
+
+    isBlue = !isBlue;
+}, 1000)
