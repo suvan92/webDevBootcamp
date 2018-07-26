3f36722f570699a7c29b6bfc07ace69eaf84d8f2
Suvan <suvanramani@gmail.com>
2018-07-26 12:36:17 -0700

    Set up routes

--- unit13_express/app.js
+++ unit13_express/app.js
@@ -1,2 +1,16 @@
 var express = require('express');
 var app = express();
+
+app.get("/", function(req, res){
+    res.send("Hi there!");
+});
+
+app.get("/bye", function(req, res){
+    res.send("Goodbye!");
+});
+
+app.get("/dog", function(req, res){
+    res.send("MEOW!!!");
+});
+
+app.listen(8080);
