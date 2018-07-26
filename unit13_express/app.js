b1c7961c1ace13ddd46606f86b302b18ab03ba17
Suvan <suvanramani@gmail.com>
2018-07-26 13:41:32 -0700

    * route and parameter encoding

--- unit13_express/app.js
+++ unit13_express/app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+// ORDER OF ROUTES MATTERS
+// First route that matches a given request is the only route that will be run
+
 app.get("/", function(req, res){
     res.send("Hi there!");
 });
@@ -13,4 +16,15 @@ app.get("/dog", function(req, res){
     res.send("MEOW!!!");
 });
 
-app.listen(8080);
+// route parameters
+app.get("/r/:name", function(req, res){
+    var name = req.params.name;
+    res.send("Welcome to r/" + name);
+});
+
+app.get("*", function(req, res){
+    res.status(404);
+    res.send("Not found :(");
+})
+
+app.listen(3000);
