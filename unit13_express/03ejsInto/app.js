7998be32c2fd5a3f7f965974ec6b06f73dc6f586
Suvan <suvanramani@gmail.com>
2018-07-27 12:13:08 -0700

    Apply styles and include header/footer

--- unit13_express/03ejsInto/app.js
+++ unit13_express/03ejsInto/app.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.render("home.ejs");
+    res.render("home");
 })
 
 app.get("/fellinlovewith/:subject", function(req, res){
     var subject = req.params.subject;
-    res.render("love.ejs", {subject: subject});
+    res.render("love", {subject: subject});
 });
 
 app.get("/posts", function(req, res){
@@ -16,7 +19,7 @@ app.get("/posts", function(req, res){
         {title: "Pet bunnies", author: "Jennifer"},
         {title: "USD", author: "Deep"},
     ]
-    res.render("posts.ejs", {posts: posts});
+    res.render("posts", {posts: posts});
 })
 
 app.listen(3000, function(){
