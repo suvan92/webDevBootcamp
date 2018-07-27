4d50043baef355dcb72b392a4924609adb704ab7
Suvan <suvanramani@gmail.com>
2018-07-27 11:18:52 -0700

    Add posts and control flow logic in ejs

--- unit13_express/03ejsInto/app.js
+++ unit13_express/03ejsInto/app.js
@@ -2,13 +2,22 @@ var express = require('express');
 var app = express();
 
 app.get("/", function(req, res){
-    res.send("This is the homepage");
+    res.render("home.ejs");
 })
 
 app.get("/fellinlovewith/:subject", function(req, res){
     var subject = req.params.subject;
     res.render("love.ejs", {subject: subject});
 });
+
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Post1", author: "Ashton"},
+        {title: "Pet bunnies", author: "Jennifer"},
+        {title: "USD", author: "Deep"},
+    ]
+    res.render("posts.ejs", {posts: posts});
+})
 
 app.listen(3000, function(){
     console.log("Listening on port: 3000");
